bcd4272027bee2f53657baf9eeb325733f632e33
David <david_why@outlook.com>
2021-03-14 17:37:52 +0800

    feat: Use `install` instead of `cp` in install script

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -pv ~/.local/lib/python3.8/site-packages/stack-calc
-cp -v *.py ~/.local/lib/python3.8/site-packages/stack-calc
+install -dvm755 ~/.local/lib/python3.8/site-packages/stack-calc
+install -v *.py ~/.local/lib/python3.8/site-packages/stack-calc
