eeef7ea77386568b21ebb122362d5c0856188841
David <david_why@outlook.com>
2021-03-14 15:18:54 +0800

    feat: Add install/uninstall scripts

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -pv ~/.local/lib/python3.8/site-packages/stack-calc
+cp -v *.py ~/.local/lib/python3.8/site-packages/stack-calc
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rm -rv ~/.local/lib/python3.8/site-packages/stack-calc
